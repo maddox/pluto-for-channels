243fb7bd3e68007e7e5c9eb416b317ea3ebdb7ee
Jon Maddox <jon@jonmaddox.com>
2021-01-15 16:26:16 -0500

    move all the versions of feeds created

--- entrypoint.sh
+++ entrypoint.sh
@@ -40,8 +40,8 @@ do
   -e "s/UPDATE_AVAILABLE/$UPDATE_AVAILABLE/g" \
   index.html > "$NGINX_ROOT/index.html"
 
-  mv playlist.m3u "$NGINX_ROOT/playlist.m3u"
-  mv epg.xml "$NGINX_ROOT/epg.xml"
+  mv *playlist.m3u "$NGINX_ROOT"
+  mv *epg.xml "$NGINX_ROOT"
   echo "Last ran: $LAST_RAN"
   sleep 10800 # run every 3 hours
 done
